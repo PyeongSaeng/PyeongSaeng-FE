6a4d5977810e55f20ef82803758cb874d17780fd
joobogyeong <powerbodol01@gmail.com>
2025-07-03 02:21:20 +0900

    chore: font setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <div className="bg-blue-900">Hello world</div>;
+  return <div className="bg-blue-400 text-5xl" >Hello world 폰트 적용 확인</div>;
 };
 
 export default App;
