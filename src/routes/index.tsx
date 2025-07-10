c664b4da903badeef4a322bd8183cd19793b237b
bomii1 <202103076@hufs.ac.kr>
2025-07-10 14:06:40 +0900

    feat: 페이지 기본 라우팅 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,20 +4,21 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<></>}></Route>
-      <Route path="/jobs/recommend" element={<></>}></Route>
-      <Route path="/jobs/recommend/:id" element={<></>}></Route>
-      <Route path="/jobs/recommend/:id/apply" element={<></>}></Route>
       <Route path="/join" element={<></>}></Route>
       <Route path="/join/senior" element={<></>}></Route>
       <Route path="/join/guardian" element={<></>}></Route>
       <Route path="/join/company" element={<></>}></Route>
       <Route path="/login" element={<></>}></Route>
+      <Route path="/jobs/recommend" element={<></>}></Route>
+      <Route path="/jobs/recommend/:id" element={<></>}></Route>
+      <Route path="/jobs/recommend/:id/apply" element={<></>}></Route>
+      <Route path="/jobs/recommend/:id/apply/question" element={<></>}></Route>
       <Route path="/jobs/saved" element={<></>}></Route>
       <Route path="/jobs/drafts" element={<></>}></Route>
-      <Route path="/questions" element={<></>}></Route>
       <Route path="/my" element={<></>}></Route>
-      <Route path="/my/jobs/applied" element={<></>}></Route>
-      <Route path="/my/jobs/applied/:id" element={<></>}></Route>
+      <Route path="/my/applied" element={<></>}></Route>
+      <Route path="/my/applied/:id" element={<></>}></Route>
+      <Route path="/my/applied/results" element={<></>}></Route>
       <Route path="/my/info" element={<></>}></Route>
       <Route path="/my/delete-account" element={<></>}></Route>
     </Routes>
