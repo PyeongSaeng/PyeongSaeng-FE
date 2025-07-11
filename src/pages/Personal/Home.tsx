ae6df7a755e50a0fe92191081ee38365720ebbb6
bomii1 <202103076@hufs.ac.kr>
2025-07-11 16:15:05 +0900

    feat: 상단바 children 작업

--- src/pages/Personal/Home.tsx
+++ src/pages/Personal/Home.tsx
@@ -3,7 +3,7 @@ import Topbar from '../../shared/components/topbar/Topbar';
 const Home = () => {
   return (
     <>
-      <Topbar />
+      <Topbar>{/* 여기에 콘텐츠 */}</Topbar>
     </>
   );
 };
--- src/shared/components/topbar/MenuButton.tsx
+++ src/shared/components/topbar/MenuButton.tsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import { CiMenuBurger } from 'react-icons/ci';
 
 const MenuButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex flex-col items-center">
-      <CiMenuBurger size={30} />
-      <span className="text-[12px]">전체 메뉴</span>
-    </div>
+    <>
+      <button className="flex flex-col items-center" onClick={() => {}}>
+        <CiMenuBurger size={30} />
+        <span className="text-[12px]">전체 메뉴</span>
+      </button>
+
+      {isOpen && (
+        <div className="z-40 flex flex-col items-start px-6 pt-20 gap-5 text-lg">
+          <a href="/jobs">일자리 추천</a>
+          <a href="/my/jobs/saved">일자리 저장함</a>
+          <a href="/my/jobs/applied">일자리 신청함</a>
+          <a href="/questions">질문답변</a>
+          <a href="/my/info">내 정보</a>
+          <a href="/logout">로그아웃</a>
+          <a href="/withdraw">회원탈퇴</a>
+        </div>
+      )}
+    </>
   );
 };
 
