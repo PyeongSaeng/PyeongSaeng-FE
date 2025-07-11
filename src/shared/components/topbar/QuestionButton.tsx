0d7eae70465bceaf0cda5fd8f655f51fa4bcf27b
bomii1 <202103076@hufs.ac.kr>
2025-07-11 15:44:32 +0900

    feat: 로고,워터마크

---
+++ src/shared/components/topbar/QuestionButton.tsx
@@ -0,0 +1,9 @@
+const QuestionButton = () => {
+  return (
+    <button className=" bg-[#08D485] rounded-[10.56px] w-[75.24px] h-[33px] px-[9.24px] py-[6.6px]">
+      질문답변
+    </button>
+  );
+};
+
+export default QuestionButton;
--- src/shared/components/topbar/Topbar.tsx
+++ src/shared/components/topbar/Topbar.tsx
@@ -1,21 +1,20 @@
 import { CiMenuBurger } from 'react-icons/ci';
 import Logo from './Logo';
 import ToggleButton from './ToggleButton';
+import QuestionButton from './QuestionButton';
 
 const Topbar = () => {
   return (
     <>
       <Logo />
       <div className="flex justify-center">
-        <div className="w-[318px] flex justify-between items-centers bt-[2px] border-b-[1.3px] border-[#707070]">
+        <div className="w-[318px] flex justify-between items-centers bt-[2px] border-b-[1.3px] border-[#707070] p-[3px]">
           <div className="flex justify-around items-start gap-[21px]">
             <div className="flex flex-col items-center">
               <CiMenuBurger size={30} />
               <span className="text-[12px]">메뉴</span>
             </div>
-            <button className="bg-[#08D485] rounded-[10.56px] w-[75.24px] h-[33px] px-[9.24px] py-[6.6px]">
-              질문답변
-            </button>
+            <QuestionButton />
           </div>
           <div>
             <ToggleButton />
