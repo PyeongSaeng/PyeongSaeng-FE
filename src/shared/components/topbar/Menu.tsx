69fd8fabf2f4c0db62a0f6403b51591710767c1a
bomii1 <202103076@hufs.ac.kr>
2025-07-17 03:14:50 +0900

    feat: 임시저장

--- src/shared/components/topbar/Menu.tsx
+++ src/shared/components/topbar/Menu.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { IoClose, IoMenu } from 'react-icons/io5';
 import MainMenu from './menu/MainMenu';
 import SeniorMyMenu from './menu/SeniorMyMenu';
-// import GuardianMyMenu from './menu/GuardianMyMenu';
+import GuardianMyMenu from './menu/GuardianMyMenu';
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -34,8 +34,8 @@ const Menu = () => {
           {currentMenu === 'main' ? (
             <MainMenu handleMenu={goToMyMenu} />
           ) : (
-            <SeniorMyMenu />
-            // <GuardianMyMenu />
+            // <SeniorMyMenu />
+            <GuardianMyMenu />
           )}
         </div>
       )}
