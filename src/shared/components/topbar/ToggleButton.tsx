d819488b42aee65a4a71b41895f8293b8c066c7a
bomii1 <202103076@hufs.ac.kr>
2025-07-17 17:19:52 +0900

    feat: 토글 로직 수정

--- src/shared/components/topbar/ToggleButton.tsx
+++ src/shared/components/topbar/ToggleButton.tsx
@@ -2,38 +2,52 @@ import { useState } from 'react';
 import clsx from 'clsx';
 
 const ToggleButton = () => {
-  const [isToggleOn, setIsToggleOn] = useState(true);
+  const [toggleVersion, setToggleVersion] = useState<'personal' | 'company'>(
+    'personal'
+  );
+
+  const goToPersonalVersion = () => setToggleVersion('personal');
+  const goToCompanyVersion = () => setToggleVersion('company');
 
   return (
-    <div
-      className="flex flex-row align-center"
-      onClick={() => setIsToggleOn(!isToggleOn)}
-    >
+    <div className="flex flex-row items-center">
       <div
         className={clsx(
-          'flex items-center justify-center transition-all duration-300 relative w-[80.61px] h-[28px] rounded-[16px] outline-[1px] outline-[#D3D3D3] px-0 py-0 bg-[#F5F5F5]'
+          'flex items-center justify-center relative w-[80.61px] h-[28px] rounded-[16px] outline outline-[1px] outline-[#D3D3D3] bg-[#F5F5F5]'
         )}
       >
-        <div className="absolute top-0 left-0 z-10 w-full h-full flex gap-[8.48px] items-center justify-around text-[11.88px] px-[7px]">
-          <span className={clsx(isToggleOn ? 'font-semibold' : 'font-normal')}>
-            개인
-          </span>
-          <span
-            className={clsx(
-              !isToggleOn ? 'font-semibold text-white' : 'font-normal'
-            )}
-          >
-            기업
-          </span>
-        </div>
         <div
           className={clsx(
-            'absolute top-0 left-0 z-0 trasition-transform duration-350 w-[44.12px] h-full rounded-[16px]',
-            isToggleOn
+            'absolute top-0 left-0 z-0 transition-transform duration-300 w-[44.12px] h-full rounded-[16px]',
+            toggleVersion === 'personal'
               ? 'translate-x-0 bg-[#08D485]'
               : 'translate-x-[36.49px] bg-[#0D29B7]'
           )}
         ></div>
+        <div className="absolute top-0 left-0 z-10 w-full h-full flex gap-[8.48px] items-center justify-around text-[11.88px] px-[7px]">
+          <button
+            onClick={goToPersonalVersion}
+            className={clsx(
+              'transition-colors duration-200',
+              toggleVersion === 'personal'
+                ? 'font-semibold'
+                : 'font-normal text-black'
+            )}
+          >
+            개인
+          </button>
+          <button
+            onClick={goToCompanyVersion}
+            className={clsx(
+              'transition-colors duration-200',
+              toggleVersion === 'company'
+                ? 'font-semibold text-white'
+                : 'font-normal text-black'
+            )}
+          >
+            기업
+          </button>
+        </div>
       </div>
     </div>
   );
