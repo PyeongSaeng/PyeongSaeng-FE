73075db0af8412838dc18421546befa24b26f03c
bomii1 <202103076@hufs.ac.kr>
2025-07-17 18:03:52 +0900

    feat: 토글 css 수정

--- src/shared/components/topbar/ToggleButton.tsx
+++ src/shared/components/topbar/ToggleButton.tsx
@@ -1,21 +1,33 @@
 import { useState } from 'react';
 import clsx from 'clsx';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const ToggleButton = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const initialVersion: 'personal' | 'company' = location.pathname.startsWith(
+    '/company'
+  )
+    ? 'company'
+    : 'personal';
+
   const [toggleVersion, setToggleVersion] = useState<'personal' | 'company'>(
-    'personal'
+    initialVersion
   );
 
-  const goToPersonalVersion = () => setToggleVersion('personal');
-  const goToCompanyVersion = () => setToggleVersion('company');
+  const goToPersonalVersion = () => {
+    setToggleVersion('personal');
+    setTimeout(() => navigate('/personal'), 400);
+  };
+  const goToCompanyVersion = () => {
+    setToggleVersion('company');
+    setTimeout(() => navigate('/company'), 400);
+  };
 
   return (
     <div className="flex flex-row items-center">
-      <div
-        className={clsx(
-          'flex items-center justify-center relative w-[80.61px] h-[28px] rounded-[16px] outline outline-[1px] outline-[#D3D3D3] bg-[#F5F5F5]'
-        )}
-      >
+      <div className="flex items-center justify-center relative w-[80.61px] h-[28px] rounded-[16px] outline outline-[1px] outline-[#D3D3D3] bg-[#F5F5F5]">
         <div
           className={clsx(
             'absolute top-0 left-0 z-0 transition-transform duration-300 w-[44.12px] h-full rounded-[16px]',
