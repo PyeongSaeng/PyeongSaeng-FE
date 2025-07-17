a61f957cd2615076e6b780397159e5de770a98d2
joobogyeong <powerbodol01@gmail.com>
2025-07-18 00:55:36 +0900

    feat: 일자리 신청함 페이지 작성 관련 버튼 추가

--- src/pages/Personal/JobDraftsPage.tsx
+++ src/pages/Personal/JobDraftsPage.tsx
@@ -9,6 +9,14 @@ const JobDraftsPage = () => {
                         일자리 신청함
                     </p>
                 </div>
+                <div className="w-[301px] flex gap-[13px] mt-[16px] justify-center">
+                    <button className="flex-1 h-[45px] border-[1.3px] border-[#08D485] rounded-[8px] text-[16px] font-medium text-black">
+                        작성 전
+                    </button>
+                    <button className="flex-1 h-[45px] border-[1.3px] border-[#08D485] rounded-[8px] text-[16px] font-medium">
+                        작성 후
+                    </button>
+                </div>
             </div>
         </Topbar>
     )
