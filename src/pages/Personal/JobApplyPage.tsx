986849ad862e6f15284ce4f7fff7568bb34dfa30
yeonsu <hazqee8@gmail.com>
2025-08-21 00:03:49 +0900

    feat(jobapply): wire updated-answers API & review flow

--- src/pages/Personal/JobApplyPage.tsx
+++ src/pages/Personal/JobApplyPage.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
-
 import Topbar from '../../shared/components/topbar/Topbar';
 import FormTitleSection from '../../shared/components/FormTitleSection';
 import JobInfoSection from '../../shared/components/JobInfoSection';
@@ -10,13 +9,11 @@ import QuestionWriteFormSection from '../../shared/components/QuestionWriteFormS
 import EvidenceSection from '../../shared/components/EvidenceSection';
 import NextButton from '../../shared/components/NextButton';
 import TwoButtonGroup from '../../shared/components/TwoButtonGroup';
-
 import {
   postGenerateAnswer,
   postGenerateUpdatedAnswer,
   type QAOption,
 } from './apis/ai';
-
 import {
   postApplicationsEnsure,
   postApplicationDirect,
@@ -25,7 +22,6 @@ import { uploadFileAndGetKey } from './apis/files';
 import { getQuestionsDirect, pickExtraFields } from './apis/questions';
 import { apiGetJobDetail } from './apis/jobapi';
 import axiosInstance from '../../shared/apis/axiosInstance';
-
 import type { FieldAndAnswer } from './types/applications';
 import type { Info } from './types/userInfo';
 
